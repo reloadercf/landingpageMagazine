e483e516dc5cbb0e97169243776896c8cb014976
Uriel Benítez <urielbenitez96@hotmail.com>
2019-10-23 16:10:54 -0500

    Responsive NavBar

--- src/components/Landing/Contextos/Contexto.jsx
+++ src/components/Landing/Contextos/Contexto.jsx
@@ -5,23 +5,24 @@ function Contexto() {
     return (
         <div className="landingPrecios">
             <Row type="flex" justify="space-around" align="middle" style={{ height: "100%" }}>
-                <Col xll={6} xl={6} md={6} sm={24} xs={24} className="CardPrecios" >
+                <Col  lg={6} xll={6} xl={6} md={20} sm={24} xs={24} className="CardPrecios" >
+                    <div id='article' className="estilo">
+                        <h4> El contexto de las aplicaciones móviles (2019)</h4>
 
-                    <h4> El contexto de las aplicaciones móviles (2019)</h4>
 
+                        <ul>
+                            <li className='contextos'> <Icon type="android" /> Hay aproximadamente 2.7 millones de aplicaciones en Google Play </li>
+                            <li className='contextos'> <Icon type="apple" /> Aplicaciones para dispositivos apple hay 1.8 millones en AppStore</li>
+                            <li className='contextos'> <Icon type="facebook" /> En Facebook hay 50 millones de paginas</li>
+                            <li className='contextos'> <Icon type="ie" /> Paginas Web existentes hay 1700 millones</li>
+                        </ul>
 
-                    <ul>
-                        <li className='contextos'> <Icon type="android" /> Hay aproximadamente 2.7 millones de aplicaciones en Google Play </li>
-                        <li className='contextos'> <Icon type="apple" /> Aplicaciones para dispositivos apple hay 1.8 millones en AppStore</li>
-                        <li className='contextos'> <Icon type="facebook" /> En Facebook hay 50 millones de paginas</li>
-                        <li className='contextos'> <Icon type="ie" /> Paginas Web existentes hay 1700 millones</li>
-                    </ul>
-
-                    <p> Si nos detenemos para analizar la estrategia de fidelizar clientes y retener audiencia de las empresas nos damos cuenta que la mayoria concentra el esfuerzo en redes sociales para dar a conocer contenido dinamico, en su mayoria no tiene obtiene resultados organicos</p>
-                    <p>Tener tu propia app permitirar captar la atencion hacia tu contenido.</p>
+                        <p> Si nos detenemos para analizar la estrategia de fidelizar clientes y retener audiencia de las empresas nos damos cuenta que la mayoria concentra el esfuerzo en redes sociales para dar a conocer contenido dinamico, en su mayoria no tiene obtiene resultados organicos</p>
+                        <p>Tener tu propia app permitirar captar la atencion hacia tu contenido.</p>
+                    </div>
                 </Col>
-                <Col xll={6} xl={6} md={6} sm={24} xs={24} className="CardPrecios" >
-                    <div id='article'>
+                <Col  lg={6} xll={6} xl={6} md={20} sm={24} xs={24} className="CardPrecios" >
+                    <div id='article' className="estilo">
                         <h4>Contexto YouMagazine</h4>
                         <p>Somos una Empresa tecnologica que busca empoderar con tecnologia a otras empresas, simplificando el proceseso tecnologico de divulagación de contenido, dando como resultado a las empresas la fidelizacion de clientes y la captación de nuevos prospectos.</p>
                         <p><center><Icon type="heart" theme="twoTone" twoToneColor="#52c41a" className='corazon' /></center></p>
@@ -30,8 +31,8 @@ function Contexto() {
                 </Col >
 
 
-                <Col xll={6} xl={6} md={6} sm={24} xs={24} className="CardPrecios" >
-                    <div id='article'>
+                <Col  lg={6} xll={6} xl={6} md={20} sm={24} xs={24} className="CardPrecios" >
+                    <div id='article' className="estilo">
                         <h4>Contexto Profesional</h4>
                         <p>Nuestra tecnologia es Robusta, Rapida y Segura.</p>
                         <p>Utilizamos lenguajes de programación modernos lo cual permite escalar cada proyecto a los niveles necesarios de cada cliente.</p>
--- src/components/Landing/Nav/Nav.jsx
+++ src/components/Landing/Nav/Nav.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
 import { Menu, Icon } from 'antd';
-
-/* function main(){
-  $('menu_bar').click(function(){
-      $('nav').toggle();
-  });
-} */
-
+import logo from '../../../assets/images/logoyoumagazine 2.png'
+import { Link } from 'react-router-dom';
 function Nav() {
   return (
-    <div>
-      <head>
-        <title>Menu</title>
-      </head>
-      <div>
+      <div className="Estilo_Header">
         <header>
           <div className="menu_bar">
-            <a href="#" className="bt-menu"><span><Icon type="unordered-list" /></span>Menu</a>
+            <a  className="bt-menu"><span><Icon type="unordered-list" /></span>Menu</a>
           </div>
           <nav className="Estilo_Nav">
             <ul>
+              <li><img src={logo} className="EstiloLogo"/></li>
               <li><a><span><Icon type="home" /></span>Home</a></li>
               <li><a><span><Icon type="tags" /></span>Caracteristicas</a></li>
               <li><a><span><Icon type="shopping" /></span>Cotiza</a></li>
@@ -31,7 +23,7 @@ function Nav() {
           </nav>
         </header>
       </div>
-    </div>
+    
   )
 }
 export default Nav
