8b77fa5ddb898cfd9b63660c8e3e216a7f01bf70
Uriel <urielbenitez96@hotmail.com>
2020-09-14 12:46:05 -0500

    first commit

---
+++ docs/menu.js
@@ -0,0 +1,7 @@
+$(document).ready(main);
+var estado= 1
+function main(){
+    $('.menu_bar').click(function(){
+        $('nav').toggle();
+    });
+}
---
+++ docs/precache-manifest.dbcceededa0c04e80857ef56fd76d5e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab195fb8a0322cab26b2f9616bc32dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b6899ebcb969af27695",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "fc09607b10b838e30e7b",
+    "url": "/static/css/main.d9efae43.chunk.css"
+  },
+  {
+    "revision": "6b6899ebcb969af27695",
+    "url": "/static/js/2.3bbcfc7e.chunk.js"
+  },
+  {
+    "revision": "fc09607b10b838e30e7b",
+    "url": "/static/js/main.70c952a7.chunk.js"
+  },
+  {
+    "revision": "2746b2eb4293bf3b4fae",
+    "url": "/static/js/runtime-main.9cb56505.js"
+  },
+  {
+    "revision": "5c18a1b3d20143caa9e629e36b4e6285",
+    "url": "/static/media/aplicacion-ejemplo-iphone-x.5c18a1b3.png"
+  },
+  {
+    "revision": "ba9eeeeb07c88a12326ff246ae4ac17c",
+    "url": "/static/media/apple.ba9eeeeb.png"
+  },
+  {
+    "revision": "b198b5323b8fc10798f811aa7a281ab5",
+    "url": "/static/media/background-del-cotizador-de-youmagazine.b198b532.jpg"
+  },
+  {
+    "revision": "55013e06a8ab0be31979dbf233448e9f",
+    "url": "/static/media/google.55013e06.png"
+  },
+  {
+    "revision": "0f3f5f3d4770b18b35a343feeb0163f1",
+    "url": "/static/media/imagen-header-youmagazine-hombre-sosteniendo-celular.0f3f5f3d.jpg"
+  },
+  {
+    "revision": "985528fa6b0e109d71b339e530b82029",
+    "url": "/static/media/logoyoumagazine 2.985528fa.png"
+  },
+  {
+    "revision": "333cf8f875b09c3420bd4ea99888f853",
+    "url": "/static/media/logoyoumagazine.333cf8f8.png"
+  }
+]);
